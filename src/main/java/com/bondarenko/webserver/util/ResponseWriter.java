d86ad36b48bf5a6047f0b4eb185e87200aa36069
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-05-29 20:57:04 +0300

    refactored ResponseWriter:
    - renamed output to socketOutputStream;
    - replace IOException to ServerException.

--- src/main/java/com/bondarenko/webserver/util/ResponseWriter.java
+++ src/main/java/com/bondarenko/webserver/util/ResponseWriter.java
@@ -1,32 +1,31 @@
 package com.bondarenko.webserver.util;
 
 import com.bondarenko.webserver.entity.HttpStatus;
+import com.bondarenko.webserver.exception.ServerException;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 
 public class ResponseWriter {
-    private final BufferedOutputStream output;
+    private final BufferedOutputStream socketOutputStream;
     private final static byte[] LINE_END = "\r\n".getBytes();
 
     public ResponseWriter(BufferedOutputStream output) {
-        this.output = output;
+        this.socketOutputStream = output;
     }
 
-    public void writeResponse(HttpStatus httpStatus, byte[] body) throws IOException {
+    public void writeResponse(HttpStatus httpStatus, byte[] body) {
         try {
-            output.write(httpStatus.getStatusLine().getBytes());
-            output.write(LINE_END);
-            if (Objects.equals(httpStatus,HttpStatus.OK)){
-                output.write(LINE_END);
-                output.write(body);
+            socketOutputStream.write(httpStatus.getStatusLine().getBytes());
+            socketOutputStream.write(LINE_END);
+            if (Objects.equals(httpStatus, HttpStatus.OK)) {
+                socketOutputStream.write(LINE_END);
+                socketOutputStream.write(body);
             }
-            output.flush();
+            socketOutputStream.flush();
         } catch (IOException ex) {
-            System.out.println("File is not written: " + ex.getMessage());
-            throw new IOException();
+            throw new ServerException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
-
